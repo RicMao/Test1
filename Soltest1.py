147adb7bc361836e6346749c8b0b36ce83fd8240
RicMao <the.elvano@gmail.com>
2024-09-13 21:33:24 +0700

    Update Soltest1.py

--- Soltest1.py
+++ Soltest1.py
@@ -1,14 +1,12 @@
 import base58
 from solders.keypair import Keypair
-import csv
 from web3 import Web3
 import time
 from colorama import Fore
 
-    # Amount of Wallets generate
-    WALLETS_AMOUNT = 10
-
     # Random generate address & private key
+    WALLETS_AMOUNT = 100
+
     for x in range(WALLETS_AMOUNT):
         account = Keypair()
         privateKey = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
@@ -17,8 +15,8 @@ from colorama import Fore
     address : account.pubkey
     private_key : privateKey
     
-    print(Fore.GREEN + f"Adr: {address}")
-    print(Fore.YELLOW + f"Key: {private_key}")
+    print(Fore.GREEN + f"Adr: (address")
+    print(Fore.YELLOW + f"Key: (private_key)")
     time.sleep(0.001)
  
     
