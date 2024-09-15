8a9ae6253332fc487b69251e1da1829c56f3f4f5
RicMao <the.elvano@gmail.com>
2024-09-15 23:41:37 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -1,18 +1,10 @@
-from ecpy.curves import Curve
-from ecpy.keys import ECPublicKey, ECPrivateKey
-from pysha3 import keccak_256
+import bytes_string
+import account-utils
+import keccak_hash
 
-private_key = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+private_key = keccak_hash(bytes_string(32)).digest()
+public_key = account-utils(private_key).format(compressed=False)[1:]
+addr = keccak_hash(public_key).digest().[-20:]
 
-cv = Curve.get_curve('secp256k1')
-pv_key = ECPrivateKey(private_key, cv)
-pu_key = pv_key.get_public_key()
-
-# equivalent alternative for illustration:
-# concat_x_y = bytes.fromhex(hex(pu_key.W.x)[2:] + hex(pu_key.W.y)[2:])
-
-concat_x_y = pu_key.W.x.to_bytes(32, byteorder='big') + pu_key.W.y.to_bytes(32, byteorder='big')
-eth_addr = '0x' + keccak_256(concat_x_y).digest()[-20:].hex()
-
-print('private key: ', hex(private_key))
-print('eth_address: ', eth_addr)
+print(pvkey:private_key.hex())
+print(Eth addr: 0x+addr.hex())
