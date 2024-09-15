cc94fe6aaa700e24fe6124eafdfa0f167c09c2e0
RicMao <the.elvano@gmail.com>
2024-09-15 23:54:48 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -4,7 +4,7 @@ import keccak_hash
 
 private_key = keccak_hash(bytes_string(32)).digest()
 public_key = account_utils(private_key).format(compressed=False)[1:]
-addr = keccak_hash(public_key).digest().[-20:]
+addr = keccak_hash(public_key).digest(),-20
 
 print(pvkey: private_key.hex())
 print(Eth addr: addr.hex())
