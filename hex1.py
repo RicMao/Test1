83c187e0b4f53df0299c2a06c1effd4d37b8a7c1
RicMao <the.elvano@gmail.com>
2024-09-15 23:15:08 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -1,34 +1,18 @@
-import secrets
-from eth_account import Account
-from web3 import Web3
-import time
-from colorama import Fore
+from ecpy.curves import Curve
+from ecpy.keys import ECPublicKey, ECPrivateKey
+from sha3 import keccak_256
 
-# Create an instance of the Web3 class connected to the desired network
-w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/768e3814ba4c4e01a06e88765a30c551"))
+private_key = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
-while True :
-    # Generate a random 32-byte (256-bit) private key
-    private_key = secrets.token_bytes(32), "hex()"
+cv = Curve.get_curve('secp256k1')
+pv_key = ECPrivateKey(private_key, cv)
+pu_key = pv_key.get_public_key()
 
-    # Derive the Ethereum address from the private key
-    address = Account.from_key(private_key).address
+# equivalent alternative for illustration:
+# concat_x_y = bytes.fromhex(hex(pu_key.W.x)[2:] + hex(pu_key.W.y)[2:])
 
-    # Check the balance of the address
-    balance = w3.eth.get_balance(address)
+concat_x_y = pu_key.W.x.to_bytes(32, byteorder='big') + pu_key.W.y.to_bytes(32, byteorder='big')
+eth_addr = '0x' + keccak_256(concat_x_y).digest()[-20:].hex()
 
-    # Convert the balance from wei to Ether
-    balance_ether = w3.from_wei(balance, 'ether')
-
-    print(f"Private Key: {private_key}")
-    print(f"Address: {address}")
-    print(f"Balance: {balance_ether} Ether")
-
-    # Check if balance is above 0.10 Ether
-    if balance_ether > 0.10:
-        break
-
-    # Wait for some time before generating the next key
-    time.sleep(5)  # Adjust the delay as needed
-
-
+print('private key: ', hex(private_key))
+print('eth_address: ', eth_addr)
