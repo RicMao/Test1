400eac40b4af9173a63bd133587d5f07bf7ee3a7
RicMao <the.elvano@gmail.com>
2024-09-15 22:15:16 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -12,7 +12,7 @@ def generate_private_key():
     private_key = secrets.token_bytes(32)
     return private_key
 
-private_key = (generate_private_key(), "hex()")
+private_key = generate_private_key(), "hex()".private_key
 
 # Infinite loop to continuously generate keys and check balances
 while True:
