3a869df4d6fa44b98d799c504f039b870b2e48e8
RicMao <the.elvano@gmail.com>
2024-09-16 21:47:25 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -1,5 +1,13 @@
-import random
+import secrets
+from eth_keys import keys
 
-hash = random.getrandbits(128)
+a=0
+while a<=1000:
+   private_key = "{:064x}".format(secrets.randbits(256))
+   private_key_bytes = bytes.fromhex(private_key)
+   public_key_hex = keys.PrivateKey(private_key_bytes).public_key
+   public_key_bytes = bytes.fromhex(str(public_key_hex)[2:])
+   public_address = keys.PublicKey(public_key_bytes).to_address()
 
-print "hash value: %032x" % hash
+   print("Key = "+private_key+" "+"Address = "+public_address)
+   a=a+1
