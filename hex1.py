47174fa1730e349b349a5c164775ac6dfeccf8aa
RicMao <the.elvano@gmail.com>
2024-09-16 23:37:23 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -1,13 +1,30 @@
 import secrets
 from eth_keys import keys
+from eth_account import Account
+from web3 import Web3
+import time
 
 a=0
-while a<=1000:
-   private_key = "{:064x}".format(secrets.randbits(256))
-   private_key_bytes = bytes.fromhex(private_key)
-   public_key_hex = keys.PrivateKey(private_key_bytes).public_key
-   public_key_bytes = bytes.fromhex(str(public_key_hex)[2:])
-   public_address = keys.PublicKey(public_key_bytes).to_address()
+while a<=100:
+    w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/768e3814ba4c4e01a06e88765a30c551"))
 
-   print("Key = "+private_key+" "+"Address = "+public_address)
-   a=a+1
+    # Generate a random private key
+    private_key = Account.create()._private_key.hex()
+
+    # Derive the Ethereum address from the private key
+    address = Account.from_key(private_key).address
+
+    # Check the balance of the address
+    balance = w3.eth.get_balance(address)
+
+    # Convert the balance from wei to Ether
+    balance_ether = w3.from_wei(balance, 'ether')
+
+    print(Fore.GREEN + f"Key: {private_key}")
+    print(Fore.YELLOW + f"Adr: {address}")
+    print(Fore.WHITE + f"Eth: {balance_ether}")
+    time.sleep(0.001)
+
+    # Check if balance is above 0.001 Ether
+    if balance_ether > 0.001: break
+    a=a+1
