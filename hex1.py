86f1240d69aa9cff2a2999a132073ea1965fd894
RicMao <the.elvano@gmail.com>
2024-09-16 10:48:05 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -1,10 +1,6 @@
-import bytes_string
-import account_utils
-import keccak_hash
+from ape import networks
+web3 = networks.provider._web3
 
-private_key = keccak_hash(bytes_string(32)).digest()
-public_key = account_utils(private_key).format(compressed=False)[1:]
-addr = keccak_hash(public_key).digest(),-20
 
-print("pvkey    :", private_key.hex())
-print("Eth addr :", addr.hex())
+print(web3.solidity_keccak(["uint8"], [0]).hex())
+print(web3.keccak(0).hex())
