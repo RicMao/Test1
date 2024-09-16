4feeec887feef04e725551d142cd75ce5da8cd1d
RicMao <the.elvano@gmail.com>
2024-09-16 23:54:21 +0700

    Update hex1.py

--- hex1.py
+++ hex1.py
@@ -5,9 +5,10 @@ from web3 import Web3
 from colorama import Fore
 import time
 
-a=0
-while a<=100:
-    w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/768e3814ba4c4e01a06e88765a30c551"))
+w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/768e3814ba4c4e01a06e88765a30c551"))
+   
+a=1
+while a<=1000:
     private_key = Account.create()._private_key.hex()
     address = Account.from_key(private_key).address
     balance = w3.eth.get_balance(address)
