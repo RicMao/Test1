0f4202236756f05ab1354b1c861d2dd449df05fd
RicMao <the.elvano@gmail.com>
2024-09-18 00:54:26 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -1,8 +1,6 @@
-import base58
-from solders.keypair import Keypair
-from theblockchainapi import SolanaAPIResource, SolanaCurrencyUnit, SolanaMintAddresses, SolanaNetwork, SolanaWallet
-from colorama import Fore
+from theblockchainapi import SolanaAPIResource, SolanaMintAddresses, SolanaNetwork, SolanaWallet
 
+# Get an API key pair for free here: https://dashboard.blockchainapi.com/api-keys
 MY_API_KEY_ID = '4ZZZFwNf4b8bumN'
 MY_API_SECRET_KEY = 'Xedr13zVPXNrFq6'
 
@@ -11,11 +9,23 @@ BLOCKCHAIN_API_RESOURCE = SolanaAPIResource(
     api_secret_key=MY_API_SECRET_KEY
 )
 
-while True :
-        account = Keypair()
-        private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
-        address = account.pubkey()
-        balance = BLOCKCHAIN_API_RESOURCE.get_balance(address)
-        print(f"SOL Balance of {address}")
-        print(balance)
-      
+
+def example():
+    try:
+        assert MY_API_KEY_ID is not None
+        assert MY_API_SECRET_KEY is not None
+    except AssertionError:
+        raise Exception("Fill in your key ID pair!")
+
+    # (1) Test get SOL balance
+    # Create a new wallet, get an airdrop, and then get its balance
+    secret_phrase = BLOCKCHAIN_API_RESOURCE.generate_secret_key()
+    print(secret_phrase)
+    wallet = SolanaWallet(secret_recovery_phrase=secret_phrase)
+    public_key = BLOCKCHAIN_API_RESOURCE.derive_public_key(wallet=wallet)
+    print(public_key)
+    airdrop_tx_signature = BLOCKCHAIN_API_RESOURCE.get_airdrop(public_key)
+    print(airdrop_tx_signature)
+    result = BLOCKCHAIN_API_RESOURCE.get_balance(public_key)
+    print(f"SOL Balance of {public_key}")
+    print(result)
