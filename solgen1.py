fd7d7d6531bf01644dc479bdb57ce19a8543cf0b
RicMao <the.elvano@gmail.com>
2024-09-17 14:33:05 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -1,15 +1,13 @@
 import base58
 from solders.keypair import Keypair
-import csv
+from colorama import Fore
 
-WALLETS_AMOUNT = 5000
-
-with open("wallets.csv", 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["WALLET", "PRIVATE KEY"])
-
-    for x in range(WALLETS_AMOUNT):
+a=0
+while a<=100:
         account = Keypair()
-        privateKey = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
-
-        csv_writer.writerow([account.pubkey(), privateKey])
+        private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
+        
+        print(Fore.GREEN + f"Key: {private_key}")
+        print(Fore.YELLOW + f"Adr: {account.pubkey}")
+        a=a+1
+      
