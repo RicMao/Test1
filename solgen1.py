7dd1d4437eae36b8e096b2d0275cc89f0140ef4a
RicMao <the.elvano@gmail.com>
2024-09-17 20:45:51 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -1,14 +1,25 @@
 import base58
 from solders.keypair import Keypair
+from theblockchainapi import SolanaAPIResource, SolanaMintAddresses, SolanaNetwork, SolanaWallet
 from colorama import Fore
+
+MY_API_KEY_ID = None
+MY_API_SECRET_KEY = None
+
+BLOCKCHAIN_API_RESOURCE = SolanaAPIResource(
+    api_key_id=MY_API_KEY_ID,
+    api_secret_key=MY_API_SECRET_KEY
+)
 
 a=0
 while a<=100:
         account = Keypair()
         private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
         address = account.pubkey()
+        balance = BLOCKCHAIN_API_RESOURCE.get_balance(address)
         
         print(Fore.GREEN + f"Key: {private_key}")
         print(Fore.YELLOW + f"Adr: {address}")
+        print(Fore.WHITE + f"Sol: {balance}")
         a=a+1
       
