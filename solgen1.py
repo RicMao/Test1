e96609df9c72b403ecf56c4adef123c50413edfa
RicMao <the.elvano@gmail.com>
2024-09-17 14:39:39 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -6,8 +6,9 @@ a=0
 while a<=100:
         account = Keypair()
         private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
+        address = account.pubkey()
         
         print(Fore.GREEN + f"Key: {private_key}")
-        print(Fore.YELLOW + f"Adr: {account.pubkey}")
+        print(Fore.YELLOW + f"Adr: {address}")
         a=a+1
       
