9a4fd4f051be2266017ede1737887010861009b9
RicMao <the.elvano@gmail.com>
2024-09-18 14:08:07 +0700

    Update solgen.py

--- solgen.py
+++ solgen.py
@@ -1,17 +1,16 @@
 import base58
 from solders.keypair import Keypair
+import csv
 
 WALLETS_AMOUNT = 100
 
-f = open('Wallet.txt', 'w')
-         
+with open("wallets.csv", 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["WALLET", "PRIVATE KEY"])
+
     for x in range(WALLETS_AMOUNT):
         account = Keypair()
         privateKey = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
         address = account.pubkey()
 
-        print(f"Key: {privateKey}")
-        print(f"Adr: {address}")
-        
-  f.write('address', 'privateKey')
-  f.close()
+        csv_writer.writerow([address, privateKey])
