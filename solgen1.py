b1cd843f7aa8198925f4e75ae7bdee21023fa52c
RicMao <the.elvano@gmail.com>
2024-09-18 09:14:53 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -1,5 +1,6 @@
 import base58
 from solders.keypair import Keypair
+from moralis import sol_api
 from colorama import Fore
 
 a=0
@@ -8,7 +9,21 @@ while a<=10:
     account = Keypair()
     private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
     address = account.pubkey()
-    
+
+
+    api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjMxN2UyNjdmLTU1YmUtNGM0OS04ZTI3LTZlOWQ0YWVjZTM0OCIsIm9yZ0lkIjoiNDA4MjU5IiwidXNlcklkIjoiNDE5NTA5IiwidHlwZUlkIjoiZDRmMWJmYjAtOGNkZS00YzY2LWJiNWQtM2VmZTE5NzhmMWY0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjYyMTg5MTUsImV4cCI6NDg4MTk3ODkxNX0.C2hqVGCoHD1gYJQLX8IQVaWpLpzfEJbRuPDONVB7uck"
+
+params = {
+    "addr": "address",
+    "network": "mainnet",
+}
+
+result = sol_api.account.balance(
+    api_key=api_key,
+    params=params,
+)
+
     print(Fore.GREEN + f"Key: {private_key}")
     print(Fore.YELLOW + f"Adr: {address}")
+    print(result)
     a=a+1
