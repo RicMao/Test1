2937bf8c3d468a106f53c67702d33a6599965905
RicMao <the.elvano@gmail.com>
2024-09-18 15:36:10 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -2,11 +2,8 @@ import base58
 from solders.keypair import Keypair
 from colorama import Fore
 
-
 a=0
-filename = 'woles.txt'
-outfile = open(filename, 'w')
-while a<=10:
+while a<=10000:
     # Random generate address & private key 
     account = Keypair()
     private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
@@ -14,7 +11,4 @@ while a<=10:
     
     print(Fore.GREEN + f"Key: {private_key}")
     print(Fore.YELLOW + f"Adr: {address}")
-a=a+1
-outfile.write("Key: {private_key} + Adr: {address}\n")
-outfile.close() #Close the file when we’re done!
-
+    a=a+1
