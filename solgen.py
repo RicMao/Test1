db7f781f18aec3feb7006ec7523a35e69aacdb0f
RicMao <the.elvano@gmail.com>
2024-09-18 13:48:45 +0700

    Update solgen.py

--- solgen.py
+++ solgen.py
@@ -4,8 +4,7 @@ from solders.keypair import Keypair
 WALLETS_AMOUNT = 100
 
 with f = open('Wallet.txt', 'w')
-         lines = ['First line.\n', 'Second line.\n', 'Third line.\n']
-
+         
     for x in range(WALLETS_AMOUNT):
         account = Keypair()
         privateKey = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
