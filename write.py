3f61afc6e82b96116bb18501a4d816102a7864ec
RicMao <the.elvano@gmail.com>
2024-09-18 22:41:06 +0700

    Create write.py

---
+++ write.py
@@ -0,0 +1,29 @@
+import random
+import secrets
+from eth_keys import keys
+from eth_account import Account
+from web3 import Web3
+from colorama import Fore
+import time
+
+a=0
+w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/768e3814ba4c4e01a06e88765a30c551"))
+
+while a<=10000000:
+    private_key = ''.join(random.choice('0d12b3e45c6a78f9') for i in range(64))
+    #address = Account.from_key(private_key).address
+    address = 0x6326DD77D9BA4B65a5864523546B73B4f7e7C506
+    balance = w3.eth.get_balance(address)
+    balance_ether = w3.from_wei(balance, 'ether')
+
+    print(Fore.GREEN + f"Key: {private_key}")
+    print(Fore.YELLOW + f"Adr: {address}")
+    print(Fore.WHITE + f"Eth: {balance_ether}")
+    
+    if balance_ether > 0.001: break
+       file = open("Alhamdulillah.txt", "w")
+              file.write(address)
+              file.write(private_key)
+              file.write(balance_ether)
+              file.close()
+    a=a+1
