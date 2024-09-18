50e1517eb5ec73abb55461a11eb558b780b7a9bb
RicMao <the.elvano@gmail.com>
2024-09-19 01:34:14 +0700

    Update random6.py

--- random6.py
+++ random6.py
@@ -19,5 +19,10 @@ while a<=10000000:
     print(Fore.YELLOW + f"Adr: {address}")
     print(Fore.WHITE + f"Eth: {balance_ether}")
     
-    if balance_ether > 0.001: break
+    f = open("Found6.txt","w")
+    if balance_ether > 0.001: 
+       f.write(private_key)
+       f.write(address)
+       f.close()
+       break
     a=a+1
