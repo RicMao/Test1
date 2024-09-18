11de513b52c2e6f773274d4134a78b42a18f789c
RicMao <the.elvano@gmail.com>
2024-09-18 10:06:35 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -1,28 +1,23 @@
 import base58
 from solders.keypair import Keypair
-from moralis import sol_api
+from MoralisSDK import ipfs
+moralis = ipfs.IPFS()
+from MoralisSDK import api
+moralis = api.MoralisApi()
+moralis.set_api_key("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjMxN2UyNjdmLTU1YmUtNGM0OS04ZTI3LTZlOWQ0YWVjZTM0OCIsIm9yZ0lkIjoiNDA4MjU5IiwidXNlcklkIjoiNDE5NTA5IiwidHlwZUlkIjoiZDRmMWJmYjAtOGNkZS00YzY2LWJiNWQtM2VmZTE5NzhmMWY0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjYyMTg5MTUsImV4cCI6NDg4MTk3ODkxNX0.C2hqVGCoHD1gYJQLX8IQVaWpLpzfEJbRuPDONVB7uck"
 from colorama import Fore
 
-api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjMxN2UyNjdmLTU1YmUtNGM0OS04ZTI3LTZlOWQ0YWVjZTM0OCIsIm9yZ0lkIjoiNDA4MjU5IiwidXNlcklkIjoiNDE5NTA5IiwidHlwZUlkIjoiZDRmMWJmYjAtOGNkZS00YzY2LWJiNWQtM2VmZTE5NzhmMWY0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjYyMTg5MTUsImV4cCI6NDg4MTk3ODkxNX0.C2hqVGCoHD1gYJQLX8IQVaWpLpzfEJbRuPDONVB7uck"
-    
 a=0
 while a<=10:
     # Random generate address
+    account = Keypair()
     address = account.pubkey()
     # Random generate private key
-    account = Keypair()
     private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
     # Get native balance of an address
-    params = {
-    "network": "mainnet",
-    "addr": "(address)",
-    }
-    balance = sol_api.account.balance(
-    api_key=api_key,
-    params=params,
-    )
+    balance = moralis.get_native_balance(address, SOL)
 
-    print(Fore.GREEN + f"Key: {private_key}")
-    print(Fore.YELLOW + f"Adr: {address}")
+    print(f"Key: {private_key}")
+    print(+ f"Adr: {address}")
     print(f"Bal: {balance}")
     a=a+1
