5b62da32b7627dadea05025e166f6048699d68b1
RicMao <the.elvano@gmail.com>
2024-09-19 09:30:40 +0700

    Update random1.py

--- random1.py
+++ random1.py
@@ -20,10 +20,11 @@ while a<=10000000:
     print(Fore.WHITE + f"Eth: {balance_ether}")
     a=a+1
 
+    f = open("hack.txt","w")
     if balance_ether > 0.001: 
-       open("hack.txt","a")
-       write(private_key)
-       write(address)
-       close()
+       f.write('(private_key)\n')
+       f.write('(address)\n')
+       f.write('str(balance_ether)\n')
+       f.close()
        break
        
