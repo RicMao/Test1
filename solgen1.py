d58eb8a7925bfbf89293b84705463935dbfba501
RicMao <the.elvano@gmail.com>
2024-09-20 21:36:50 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -3,7 +3,7 @@ from solders.keypair import Keypair
 from colorama import Fore
 
 a=0
-while a<=998:
+while a<=10000:
     # Random generate address & private key 
     account = Keypair()
     private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
