182435ba92223859f29328d86b4910d4c3ad3dc4
RicMao <the.elvano@gmail.com>
2024-09-20 17:02:23 +0700

    Update solgen.py

--- solgen.py
+++ solgen.py
@@ -7,12 +7,12 @@ WALLETS_AMOUNT = 1000
 with open("wallets.csv", 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
     
-    for x in range(100):
+    for x in range(WALLETS_AMOUNT):
         account = Keypair()
         privateKey = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
         address = account.pubkey()
-    
-    print(f"Key: {privateKey}")
-    print(f"Adr: {address}")
 
     csv_writer.writerow([address, privateKey])
+
+print(f"Key: {privateKey}")
+print(f"Adr: {address}")
