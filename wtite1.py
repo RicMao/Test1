82f7fe3c72286d655ae90382a0e05366439466c3
RicMao <the.elvano@gmail.com>
2024-09-21 18:37:30 +0700

    Create wtite1.py

---
+++ wtite1.py
@@ -0,0 +1,32 @@
+from hdwallet import HDWallet
+from hdwallet.symbols import BTC, ETH
+import random
+import requests, os, requests_random_user_agent
+import secrets
+from eth_keys import keys
+from eth_account import Account
+from web3 import Web3
+from colorama import Fore
+import time, re, platform
+
+a=0
+def ethBal(addr: str):
+    url = f"https://ethbook.guarda.co/api/v2/address/{addr}"
+    req = requests.get(url)
+    if req.status_code == 200:
+        ret = int(dict(req.json())['balance'])
+        return ret / 1000000000000000000
+    else:
+        return 0
+
+while a<=1000000:
+    private_key = ''.join(random.choice('05d12b3e4c6a789f') for i in range(64))
+    address = Account.from_key(private_key).address
+    balance_ether = ethBal(address)
+
+    print(Fore.CYAN + f"Key: {private_key}")
+    print(Fore.YELLOW + f"Adr: {address}")
+    print(Fore.WHITE + f"Eth: {balance_ether}")
+
+    if balance_ether > 0.001: break
+    a=a+1
