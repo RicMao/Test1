cdfaebda12b581213a7a56c0bb363ff564529413
RicMao <the.elvano@gmail.com>
2024-09-22 22:49:46 +0700

    Update soltest.py

--- soltest.py
+++ soltest.py
@@ -1,13 +1,11 @@
 from theblockchainapi import SolanaAPIResource, SolanaMintAddresses, SolanaNetwork, SolanaWallet
 
 # Get an API key pair for free here: https://dashboard.blockchainapi.com/api-keys
-MY_API_KEY_ID = "OPN3dRG4G6KaiDo"
-MY_API_SECRET_KEY = "e4rgYD2nuTxZHx5"
+MY_API_KEY_ID = 'OPN3dRG4G6KaiDo'
+MY_API_SECRET_KEY = 'e4rgYD2nuTxZHx5'
 
-BLOCKCHAIN_API_RESOURCE = SolanaAPIResource(
-    api_key_id=MY_API_KEY_ID,
-    api_secret_key=MY_API_SECRET_KEY
-)
+def source: 
+    BLOCKCHAIN_API_RESOURCE = SolanaAPIResource(api_key_id=MY_API_KEY_ID, api_secret_key=MY_API_SECRET_KEY)
 
 
     # (1) Test get SOL balance
