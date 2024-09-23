264baa6ae8e0ad67327eaea5214a3f310903b3dc
RicMao <the.elvano@gmail.com>
2024-09-23 21:35:46 +0700

    Update random10.py

--- random10.py
+++ random10.py
@@ -7,10 +7,10 @@ from colorama import Fore
 import time
 
 a=0
-w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/42dc60b1b5364af580af0c203d4c7731"))
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/42dc60b1b5364af580af0c203d4c7731'))
 
 while a<=10000000:
-    private_key = ''.join(random.choice('01fb243c56879ade') for i in range(64))
+    private_key = "0x" + ''.join(random.choice('01fb243c56879ade') for i in range(64))
     address = Account.from_key(private_key).address
     balance = w3.eth.get_balance(address)
     balance_ether = w3.from_wei(balance, 'ether')
