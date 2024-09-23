6bd6bd8c4763ea84516ca1ea9f97ecded46d66f2
RicMao <the.elvano@gmail.com>
2024-09-23 21:38:06 +0700

    Update write2.py

--- write2.py
+++ write2.py
@@ -11,7 +11,7 @@ import time, re, platform
 
 a=0
 while a<=1000000:
-    private_key = ''.join(random.choice('053d12be4c6a978f') for i in range(64))
+    private_key = "0x" + ''.join(random.choice('053d12be4c6a978f') for i in range(64))
     address = Account.from_key(private_key).address
     api_key = 'NM227DPG297EW1EUKXIH39X7PY1F13SNTN'
     url = f'https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={api_key}'
