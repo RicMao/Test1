5d279343e4c6fc56fa8f4caf88dcaeb5e1112926
RicMao <the.elvano@gmail.com>
2024-09-24 14:44:02 +0700

    Update random7.py

--- random7.py
+++ random7.py
@@ -12,7 +12,7 @@ w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/fd72f605f5df4777a61a86
 while a<=10000000:
     private_key = "0x" + ''.join(random.choice('01fb234c56879ade') for i in range(64))
     address = Account.from_key(private_key).address
-    balance = w3.eth.getBalance(address)
+    balance = w3.eth.get_balance(address)
     balance_ether = w3.from_wei(balance, 'ether')
 
     print(Fore.CYAN + f"Key: {private_key}")
