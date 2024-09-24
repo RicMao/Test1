fdb133dc8fc94d384ab6f85dc733dcace2a40ac5
RicMao <the.elvano@gmail.com>
2024-09-25 00:01:23 +0700

    Update write1.py

--- write1.py
+++ write1.py
@@ -25,9 +25,10 @@ def ethBal(address: str):
     req = requests.get(url)
     if req.status_code == 200: 
         ret = int(dict(req.json())['balance'])
-       return ret / 10 ** 18
+       return ret / (10 ** 18)
     else:
         return 0
+        
 a=0
 z=1
 ff=0
