9ea0fe8979fe09b6549a6cbb3f4da844d90cec41
RicMao <the.elvano@gmail.com>
2024-09-25 11:22:42 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -14,11 +14,10 @@ yellow = Colors.YELLOW
 reset = Colors.RESET
 getClear()
 # ------------------------------------------------------------------------
-z=1
 a=0
-
-while a>=100:
-    # Random generate address & private key 
+z=1
+while a<=100:
+    # Random generate address & private key
     account = Keypair()
     private_key = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
     address = account.pubkey()
