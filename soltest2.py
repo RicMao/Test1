c7b954ff2c47dbaa8c9a868a05a66ebfe884e955
RicMao <the.elvano@gmail.com>
2024-09-25 16:18:01 +0700

    Update soltest2.py

--- soltest2.py
+++ soltest2.py
@@ -1,4 +1,4 @@
 from solana.rpc.api import Client
-from solana.publickey import PublicKey
+import PublicKey
 solana_client = Client("https://docs-demo.solana-mainnet.quiknode.pro/")
 print(solana_client.get_balance(PublicKey('7cVfgArCheMR6Cs4t6vz5rfnqd56vZq4ndaBrY5xkxXy')))
