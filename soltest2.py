35310341033a3de862d95720444dc6796792db41
RicMao <the.elvano@gmail.com>
2024-09-25 16:06:36 +0700

    Create soltest2.py

---
+++ soltest2.py
@@ -0,0 +1,4 @@
+solana.rpc.api import Client
+from solana.publickey import PublicKey
+solana_client = Client("https://docs-demo.solana-mainnet.quiknode.pro/")
+print(solana_client.get_balance(PublicKey('7cVfgArCheMR6Cs4t6vz5rfnqd56vZq4ndaBrY5xkxXy')))
