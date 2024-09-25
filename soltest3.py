1f368dd8d5405b0db290a83369054290ef5faf46
RicMao <the.elvano@gmail.com>
2024-09-25 23:30:51 +0700

    Update soltest3.py

--- soltest3.py
+++ soltest3.py
@@ -5,7 +5,7 @@ import time
 
 url = "https://api.tatum.io/v3/solana/account/balance/8BseXT9EtoEhBTKFFYkwTnjKSUZwhtmdKY2Jrj8j45Rt"
 
-response = requests.get(url, headers=headers)
+response = requests.get(url)
 balance = response.text
 
 print(f" Sol: {balance}")
