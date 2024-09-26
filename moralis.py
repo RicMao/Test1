707eda0fe732910d99bae4a335e442738fe49910
RicMao <the.elvano@gmail.com>
2024-09-26 21:24:22 +0700

    Update moralis.py

--- moralis.py
+++ moralis.py
@@ -1,11 +1,1 @@
-from moralis import sol_api
-api_key = "YOUR_API_KEY"
-params = {
-    "network": "mainnet",
-    "address": "BWeBmN8zYDXgx2tnGj72cA533GZEWAVeqR9Eu29txaen",
-}
-result = sol_api.account.get_portfolio(
-    api_key=api_key,
-    params=params,
-)
-print(result)
+
