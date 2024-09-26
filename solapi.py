0710c60ea6ca43dae0af162c8ca033458d12f565
RicMao <the.elvano@gmail.com>
2024-09-27 00:27:47 +0700

    Update solapi.py

--- solapi.py
+++ solapi.py
@@ -1,9 +1,9 @@
 from solana.rpc.api import Client
 
-address = '52C9T2T7JRojtxumYnYZhyUmrN7kqzvCLc4Ksvjk7TxD'
+#address = '52C9T2T7JRojtxumYnYZhyUmrN7kqzvCLc4Ksvjk7TxD'
 mainnet_beta_url = 'https://api.mainnet-beta.solana.com'
 solana_client = Client(mainnet_beta_url)
 
-balance = solana_client.get_balance(address)['result']['value']
+balance = solana_client.get_balance('52C9T2T7JRojtxumYnYZhyUmrN7kqzvCLc4Ksvjk7TxD')['result']['value']
 ui_balance = round(balance*10**(-9), 9)
 print(ui_balance)
