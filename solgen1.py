9de59e2ef0892ce3f84d3f697c71b702c355e5b6
RicMao <the.elvano@gmail.com>
2024-09-26 12:38:47 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -14,25 +14,15 @@ yellow = Colors.YELLOW
 reset = Colors.RESET
 getClear()
 # ------------------------------------------------------------------------
-z=1
-while z<=10:
+z=0
+while z<=666:
     # Random generate address & private key
     account = Keypair()
     
     private = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
     #address = account.pubkey()
-    address = account.pubkey().decode('utf-8')
-
-    url = "https://api.tatum.io/v3/solana/account/balance/address"
-    headers = {
-    "accept": "application/json",
-    "x-api-key": "t-66f3de3e6be651758a55cd61-5d1a4df4209c4956afa40401"
-    }
-    response = requests.get(url, headers=headers)
-    
+    address = account.pubkey()
+    z+=1
     
     print(Fore.GREEN + f"Key: {private}")
     #print(Fore.YELLOW + f"Adr: {address}")
-    print(response.text)
-    
-z+=1
