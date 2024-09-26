f2b2dd74cd32d535e577fb0ff4031ffff8298509
RicMao <the.elvano@gmail.com>
2024-09-26 09:59:18 +0700

    Update soltest3.py

--- soltest3.py
+++ soltest3.py
@@ -1,10 +1,16 @@
-import TatumSDK, Network, Solana
+import requests
 
-tatum = await TatumSDK.init<Solana>({
-  network: Network.SOLANA,
-  apiKey: 't-66a730ccccfd17001c479705-2f597d14ad7543f289a03418',
-})
+url = 'https://solana-mainnet.gateway.tatum.io/'
+headers = {
+  'accept': 'application/json',
+  'content-type': 'application/json',
+  'x-api-key': 't-66a730ccccfd17001c479705-2f597d14ad7543f289a03418'
+}
+body = {
+  'jsonrpc': '2.0',
+  'method': 'getVersion',
+  'id': 1
+}
 
-res = await tatum.rpc.getBalance('8Ew6iQXcTRHAUNNu3X9VBn1g1bJkXEZJ9gFD2AGKtdPB')    
-    
-await tatum.destroy()
+response = requests.post(url, headers=headers, json=body)
+print(response.text)
