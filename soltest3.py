9f5cfc0ca5157196804be2640e3ad28b84c38b20
RicMao <the.elvano@gmail.com>
2024-09-26 09:55:29 +0700

    Update soltest3.py

--- soltest3.py
+++ soltest3.py
@@ -1,10 +1,10 @@
-import { TatumSDK, Network, Solana } from '@tatumio/tatum'
+import TatumSDK, Network, Solana
 
-const tatum = await TatumSDK.init<Solana>({
+tatum = await TatumSDK.init<Solana>({
   network: Network.SOLANA,
   apiKey: 't-66a730ccccfd17001c479705-2f597d14ad7543f289a03418',
 })
 
-const res = await tatum.rpc.getBalance('8Ew6iQXcTRHAUNNu3X9VBn1g1bJkXEZJ9gFD2AGKtdPB')    
+res = await tatum.rpc.getBalance('8Ew6iQXcTRHAUNNu3X9VBn1g1bJkXEZJ9gFD2AGKtdPB')    
     
 await tatum.destroy()
