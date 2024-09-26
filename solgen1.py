0bc9fe933e942fb958a5c70fc4543222528097f8
RicMao <the.elvano@gmail.com>
2024-09-26 11:31:36 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -21,9 +21,8 @@ while z<=10:
     
     private = base58.b58encode(account.secret() + base58.b58decode(str(account.pubkey()))).decode('utf-8')
     #address = account.pubkey()
-    address = 'MJKqp326RZCHnAAbew9MDdui3iCKWco7fsK9sVuZTX2'
 
-    url = "https://api.tatum.io/v3/solana/account/balance/address"
+    url = "https://api.tatum.io/v3/solana/account/balance/MJKqp326RZCHnAAbew9MDdui3iCKWco7fsK9sVuZTX2"
     headers = {
     "accept": "application/json",
     "x-api-key": "t-66f3de3e6be651758a55cd61-5d1a4df4209c4956afa40401"
