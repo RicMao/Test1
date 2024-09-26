22074c70282a82036802059c12abe9fd45a40a13
RicMao <the.elvano@gmail.com>
2024-09-26 21:50:28 +0700

    Update moralis.py

--- moralis.py
+++ moralis.py
@@ -1,1 +1,16 @@
+from moralis import sol_api
+
+api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImVlOWFkNGM1LWIxMTgtNDgyOS04NzRjLTg5OTRlYTk4MzczOCIsIm9yZ0lkIjoiNDA5NTMzIiwidXNlcklkIjoiNDIwODQzIiwidHlwZUlkIjoiOTY0YjM2YWYtYjA0Ny00MjQ2LTg3MTctNWU1NzNmOGViZTNhIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjczNTk0OTIsImV4cCI6NDg4MzExOTQ5Mn0.3I6NBMujh95Ey7cesRs8tOP_9ttOiEKHFtDMkLvaFqs"
+
+params = {
+    "address": "52C9T2T7JRojtxumYnYZhyUmrN7kqzvCLc4Ksvjk7TxD",
+    "network": "mainnet",
+}
+
+result = sol_api.account.balance(
+    api_key=api_key,
+    params=params,
+)
+
+print(result)
 
