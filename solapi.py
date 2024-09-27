5b4b6a8149b3904ab989fd43cf9cd25d6c9c728b
RicMao <the.elvano@gmail.com>
2024-09-27 09:39:59 +0700

    Update solapi.py

--- solapi.py
+++ solapi.py
@@ -10,4 +10,4 @@ solana_client = Client(mainnet_beta_url)
 payload = {"jsonrpc": "2.0", "id":"1", "method": "getBalance", "params": [wallet_address]}
 balance = post(mainnet_beta_url, json=payload).json()['result']['value']
 balance_rpc = round(balance*10**(-9), 9)
-print(f"Balance: {balance_rpc})
+print(f"Balance: {balance_rpc}")
