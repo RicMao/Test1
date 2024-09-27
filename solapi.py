82c21d3a5e1e4a0637cd6c71e28c23d1aa2a9301
RicMao <the.elvano@gmail.com>
2024-09-27 09:30:12 +0700

    Update solapi.py

--- solapi.py
+++ solapi.py
@@ -7,7 +7,7 @@ wallet_address = '52C9T2T7JRojtxumYnYZhyUmrN7kqzvCLc4Ksvjk7TxD'
 mainnet_beta_url = 'https://api.mainnet-beta.solana.com'
 solana_client = Client(mainnet_beta_url)
 
-balance = solana_client.get_balance(wallet_address)['result']['value']
+balance = solana_client.get_balance.str(wallet_address)['result']['value']
 ui_balance = round(balance*10**(-9), 9)
 print(ui_balance)
 
