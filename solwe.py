46cb5f196da901d816e1b1179d2907cb29d6d5d6
RicMao <the.elvano@gmail.com>
2024-09-27 17:14:53 +0700

    Update solwe.py

--- solwe.py
+++ solwe.py
@@ -7,8 +7,6 @@ import requests, os, requests_random_user_agent
 from colorthon import Colors
 from colorama import Fore
 
-key_array = [4,182,130,247,119,117,227,207,112,73,170,126,222,197,244,99,215,107,255,202,33,43,36,17,104,111,157,246,196,192,174,95,240,23,238,206,118,215,154,238,229,96,11,37,156,123,51,223,5,231,17,117,86,136,103,14,75,95,175,132,148,54,1,46]
-random_array = random.choices(key_array, k=64)
 
 def getClear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -22,7 +20,8 @@ getClear()
 # ------------------------------------------------------------------------
 z=0
 while z<=666:
-    # Random generate address & private key
+    key_array = [4,182,130,247,119,117,227,207,112,73,170,126,222,197,244,99,215,107,255,202,33,43,36,17,104,111,157,246,196,192,174,95,240,23,238,206,118,215,154,238,229,96,11,37,156,123,51,223,5,231,17,117,86,136,103,14,75,95,175,132,148,54,1,46]
+    random_array = random.choices(key_array, k=64)
     secret_key = random_array[0:64]
     public_key = random_array[32:64]
 
