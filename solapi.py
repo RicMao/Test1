8994f280fdbc0f4dd16e82cf2f0490ebeab47e6d
RicMao <the.elvano@gmail.com>
2024-09-27 09:32:04 +0700

    Update solapi.py

--- solapi.py
+++ solapi.py
@@ -7,11 +7,11 @@ wallet_address = '52C9T2T7JRojtxumYnYZhyUmrN7kqzvCLc4Ksvjk7TxD'
 mainnet_beta_url = 'https://api.mainnet-beta.solana.com'
 solana_client = Client(mainnet_beta_url)
 
-balance = solana_client.get_balance.str(wallet_address)['result']['value']
-ui_balance = round(balance*10**(-9), 9)
-print(ui_balance)
+#balance = solana_client.get_balance.str(wallet_address)['result']['value']
+#ui_balance = round(balance*10**(-9), 9)
+#print(ui_balance)
 
-#payload = {"jsonrpc": "2.0", "id":"1", "method": "getBalance", "params": [wallet_address]}
-#balance_rpc = post(mainnet_beta_url, json=payload).jason()['result']['value']
-#ui_balance_rpc = round(balance*10**(-9), 9)
-#print(ui_balance_rpc)
+payload = {"jsonrpc": "2.0", "id":"1", "method": "getBalance", "params": [wallet_address]}
+balance_rpc = post(mainnet_beta_url, json=payload).jason()['result']['value']
+ui_balance_rpc = round(balance*10**(-9), 9)
+print(ui_balance_rpc)
