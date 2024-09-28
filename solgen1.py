c88d329d09ff269a5d9f58efbdf303a43e3cd589
RicMao <the.elvano@gmail.com>
2024-09-28 17:40:40 +0700

    Update solgen1.py

--- solgen1.py
+++ solgen1.py
@@ -27,7 +27,7 @@ while z<=50:
     wallet_address = account.pubkey()
     z+=1
 
-    file1.write(str(wallet_address)+"\n")
+    file1.write(str(wallet_address = private)+"\n")
 
     print(Fore.GREEN + f"Key: {private}")
     print(Fore.YELLOW + f"Adr: {wallet_address}")
